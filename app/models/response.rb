41803ce1b8759d4cee7a347e66e930b43704c3ed
Charity Howell <caccheme@hotmail.com>
2013-01-10 13:47:51 -0600

    no dangling commissions when delete requests

--- app/models/response.rb
+++ app/models/response.rb
@@ -4,7 +4,7 @@ class Response < ActiveRecord::Base
   has_many :employees, :through => :requests
   
   has_many :commissions, :dependent => :destroy
-  accepts_nested_attributes_for :commissions
+  accepts_nested_attributes_for :commissions, :allow_destroy => true
 
   validates_presence_of :comment, :employee_id
 
